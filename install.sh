9e6f682cabcebfb50f20a0c8c94966da53e66d06
Ed Cunningham <ejc@tds.net>
2011-07-22 12:14:12 -0400

    Switched to the CC0 Public Domain License.

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-
+#################################################################### License ###
+# unp-inplace - a tiny wrapper script for the unarchiver unp.
+# Written in 2011 by Ed Cunningham (ejc@tds.net)
+#
+# To the extent possible under law, the author(s) have dedicated all 
+# copyright and related and neighboring rights to this software to the 
+# public domain worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see 
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+################################################################## Installer ###
 srcdir=$(dirname $(readlink -f $0))	# Find the installer's parent directory.
 
 done=false
@@ -25,7 +36,7 @@ until $done; do
 		echo 1>&2 "Uh oh. I can't seem to find the command unp."
 		echo 1>&2 "Try installing it with your favorite package manager,"
 		echo 1>&2 "then come back here."
-		exit 1; } # if not, gtfo.
+		exit 1; } # if not, exit gracefully.
 
 		# Next, install the files.
 		sudo cp -vi ${srcdir}/unp-inplace /usr/bin
